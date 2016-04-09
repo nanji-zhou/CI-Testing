8564583faa75b9df915af41d688ecac1c56faba8
unknown <Administrator@Win7-autotest.corp.contoso.com>
2016-04-09 21:21:31 +1000

    Changes back to original

--- spec/login_refactored_spec.rb
+++ spec/login_refactored_spec.rb
@@ -1,6 +1,6 @@
 load File.dirname(__FILE__) + '/../test_helper.rb'
 
-describe "User Login" do
+describe "User Login_Refactored" do
   include TestHelper
 
   before(:all) do
@@ -18,7 +18,7 @@ describe "User Login" do
     login_page = LoginPage.new(@browser)
     login_page.login("agileway", "testwise")  
     # selenium does not have browser.text yet
-    try_for(3) {  @browser.page_source.include?("Welcome").should == true }
+    try_for(3) {  @browser.page_source.include?("WelcomeX").should == true }
     @browser.find_element(:link_text, "Sign off").click
   end
 
